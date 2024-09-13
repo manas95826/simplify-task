e5d0adc7ce29ac33b05fc8bb881635fedcd5d35f
Manas Chopra <74074241+manas95826@users.noreply.github.com>
2024-09-13 22:51:16 +0530

    Create rag-app.py

---
+++ rag-app.py
@@ -0,0 +1,47 @@
+import bs4
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_chroma import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_groq import ChatGroq
+import os
+
+#Loading the content from a customer-review website
+loader = WebBaseLoader(
+    web_paths=("https://supportyourapp.com/",),
+    # bs_kwargs=dict(
+    #     parse_only=bs4.SoupStrainer(
+    #         class_=("post-content", "post-title", "post-header")
+    #     )
+    # ),
+)
+docs = loader.load()
+
+#Loading the mixtral model from groq client
+llm = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768",api_key='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')
+
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+
+# Retrieve and generate using the relevant snippets of the blog.
+retriever = vectorstore.as_retriever()
+prompt = hub.pull("rlm/rag-prompt")
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+rag_chain.invoke("Why do companies outsource customer service?")
